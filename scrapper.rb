875d8fada9d25b0f42f89ee09becf5955dd5a291
AmritDeep <amritdeepdhungana@hotmail.com>
2015-07-16 20:38:09 +0545

    allow user to enter city name and get to url

--- scrapper.rb
+++ scrapper.rb
@@ -1,1 +1,15 @@
 require "nokogiri"
+
+# puts "Enter city or zip ="
+# city_zip=gets.chomp
+city_zip="new york"
+
+city_zip=city_zip.sub(/ /, '+') if city_zip.match(/\s/)
+
+puts "#{city_zip}"
+
+# href="/rms/state/#{city_zip}.html"
+# country_href="https://therapi`sts.psychologytoday.com/rms/county/#{city_zip}/#{city_zip}.html"
+therapists_url="https://therapists.psychologytoday.com"
+state_href="https://therapists.psychologytoday.com/rms/state/#{city_zip}/#{city_zip}.html"
+puts "#{state_href}"
