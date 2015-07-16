700320759ba824e8040ceff3e2bb9b9a18e5996e
AmritDeep <amritdeepdhungana@hotmail.com>
2015-07-16 20:44:21 +0545

    initialized results array and push id and picture id in it

--- scrapper.rb
+++ scrapper.rb
@@ -21,8 +21,14 @@ url=Nokogiri::HTML(open(state_href))
 data=url.xpath('//div[@id="results-right"]')
 detail=data.xpath('//div[@class="row-fluid result-row"]')
 
+results = []
+
 detail.each do |detail|
 	id=detail['data-profid']
 	pictur_id_url= detail.css('.result-photo img').attr('src').value
 
+	results.push(
+		id: id,
+		pictur_id_url: pictur_id_url
+		)
 end
